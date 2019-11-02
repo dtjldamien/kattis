d89ac169b3c3bd7bd495f7906bf523ec264cd156
Damien Tan <dtjldamien@gmail.com>
2019-11-03 03:19:51 +0800

    simple questions

---
+++ FlyingSafely.java
@@ -0,0 +1,82 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+
+public class FlyingSafely {
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        int numTestCase = io.getInt();
+        // assuming that it is a MST, num of edges = num of vertices - 1
+        for (int i = 0; i < numTestCase; i++) {
+            int n = io.getInt(); // number of cities
+            int m = io.getInt(); // number of pilots
+            for (int j = 0; j < m; j++) {
+                int a = io.getInt(); // city a
+                int b = io.getInt(); // city b
+            }
+            io.println(n - 1);
+        }
+        io.close();
+    }
+}
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) { }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
---
+++ PieceOfCake2.java
@@ -0,0 +1,79 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+
+public class PieceOfCake2 {
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        int n = io.getInt(); // length of the sides of the square cake
+        int h = io.getInt(); // distance of the horizontal cut from the top edge of the cake
+        int v = io.getInt(); // distance of the vertical cut from the left edge of the cake
+        int topLeft = h * v;
+        int topRight = h * (n - v);
+        int bottomLeft = (n - h) * v;
+        int bottomRight = (n - h) * (n - v);
+        io.println(4 * Math.max(bottomRight, Math.max(bottomLeft, Math.max(topLeft, topRight))));
+        io.close();
+    }
+}
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null) return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) { }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
