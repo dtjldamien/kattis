5d3475355adec744ba3d681d36327052e0d5e54a
Damien Tan <dtjldamien@gmail.com>
2019-11-20 10:48:25 +0800

    Create ReachableRoads.java

---
+++ ReachableRoads.java
@@ -0,0 +1,136 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+
+public class ReachableRoads {
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        int n = io.getInt(); // number of cities
+        for (int i = 0; i < n; i++) {
+            int m = io.getInt(); // number of road endpoints
+            UnionFind ufds = new UnionFind(m);
+            int r = io.getInt(); // pairs of endpoints
+            for (int j = 0; j < r; j++) {
+                int x = io.getInt();
+                int y = io.getInt();
+                ufds.unionSet(x, y); // x and y are directly connected
+            }
+            io.println(ufds.numSets - 1); // number of roads to be added
+        }
+        io.close();
+    }
+}
+
+class UnionFind {
+    public int[] p;
+    public int[] rank;
+    public int numSets;
+
+    public UnionFind(int N) {
+        p = new int[N];
+        rank = new int[N];
+        numSets = N;
+        for (int i = 0; i < N; i++) {
+            p[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    public int findSet(int i) {
+        if (p[i] == i) {
+            return i;
+        } else {
+            p[i] = findSet(p[i]);
+            return p[i];
+        }
+    }
+
+    public boolean isSameSet(int i, int j) {
+        return findSet(i) == findSet(j);
+    }
+
+    public void unionSet(int i, int j) {
+        if (!isSameSet(i, j)) {
+            numSets--; 
+            int x = findSet(i);
+            int y = findSet(j);
+            // rank is used to keep the tree short
+            if (rank[x] > rank[y]) {
+                p[y] = x;
+            } else {
+                p[x] = y;
+                if (rank[x] == rank[y]) {
+                    rank[y] = rank[y] + 1;
+                }
+            }
+        }
+    }
+
+    public int numDisjointSets() {
+        return numSets;
+    }
+}
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null)
+                        return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) {
+            }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
