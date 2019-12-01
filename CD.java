d584681b09d5f6e008ab280a2841f7204672d362
Damien Tan <dtjldamien@gmail.com>
2019-12-01 23:29:10 +0800

    Create CD.java

---
+++ CD.java
@@ -0,0 +1,96 @@
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.util.Set;
+import java.util.HashSet;
+
+public class CD {
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        while (true) {
+            int n = io.getInt(); // number of CDs owned by Jack
+            int m = io.getInt(); // number of CDs owned by Jill
+            if (n == 0 && m == 0) {
+                break;
+            } else {
+                Set<Integer> ownedCDs = new HashSet<>(1000000);
+                int numExtraCDs = 0;
+                for (int i = 0; i < n; i++) { // Jack
+                    ownedCDs.add(io.getInt());
+                }
+                for (int i = 0; i < m; i++) { // Jill
+                    int catalogNumber = io.getInt();
+                    if (ownedCDs.contains(catalogNumber)) {
+                        numExtraCDs++;
+                    }
+                }
+                io.println(numExtraCDs);
+            }
+        }
+        io.close();
+    }
+}
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null)
+                        return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) {
+            }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
