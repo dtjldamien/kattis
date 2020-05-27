e5f6677a91fd772670f8bf0ca7358736be9b5701
Damien Tan <dtjldamien@gmail.com>
2020-05-27 12:50:03 +0800

    flexible and busnumbers

---
+++ algo.is/busnumbers.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  int n;  // number of buses
+  cin >> n;
+  bool buses[1001] = {0};  // initialise array to be all false
+  int bus_number;
+  for (int i = 0; i < n; i++) {
+    cin >> bus_number;
+    buses[bus_number] = true;
+  }
+  int consecutive_counter = 0;
+  int consecutive_end = 0;
+  for (int i = 1; i <= 1000; i++) {
+    if (buses[i]) {
+      if (consecutive_counter == 0) {
+        cout << i;
+      } else if (i == 1000) {
+        if (consecutive_counter == 1) {
+          cout << ' ' << 1000;
+        } else if (consecutive_counter > 1) {
+          cout << '-' << 1000;
+        }
+      }
+      consecutive_counter++;
+      consecutive_end = i;
+    } else {
+      if (consecutive_counter == 2) {
+        cout << ' ' << consecutive_end << ' ';
+      } else if (consecutive_counter > 2) {
+        cout << '-' << consecutive_end << ' ';
+      } else if (consecutive_counter == 1) {
+          cout << ' ';
+      }
+      consecutive_counter = 0;
+    }
+  }
+  cout << endl;
+}
---
+++ algo.is/flexible.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int width;  // width of room
+  cin >> width;
+  int p;  // number of partitions
+  cin >> p;
+  vector<int> locations;
+  int location;
+  locations.push_back(0);
+  for (int i = 0; i < p; i++) {
+    cin >> location;
+    locations.push_back(location);
+  }
+  locations.push_back(width);
+
+  vector<int> feasible_widths;
+  for (int i = 0; i < locations.size() - 1; i++) {
+    for (int j = i + 1; j < locations.size(); j++) {
+      feasible_widths.push_back(locations[j] - locations[i]);
+    }
+  }
+  sort(feasible_widths.begin(), feasible_widths.end());
+  vector<int>::iterator itr;
+  itr = unique(feasible_widths.begin(), feasible_widths.end());
+  feasible_widths.resize(distance(feasible_widths.begin(), itr));
+  for (auto feasible_width : feasible_widths) {
+      cout << feasible_width << " ";
+  }
+  cout << endl;
+}
