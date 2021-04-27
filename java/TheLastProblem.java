b285f435ec7c95d10f5defe3840b23bfdc6c2e87
Damien Tan <dtjldamien@gmail.com>
2021-04-27 18:13:56 +0800

    feat: TheLastProblem

---
+++ java/TheLastProblem.java
@@ -0,0 +1,23 @@
+package java;
+
+// https://open.kattis.com/problems/thelastproblem
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+
+public class TheLastProblem {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            PrintWriter pw = new PrintWriter(System.out);
+            String input = br.readLine();
+            pw.printf("Thank you, %s, and farewell!\n", input);
+            pw.close();
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
