627e6e9d1fea79fefbcb0b6f354f5b00472ea9bf
dtjldamien <dtjldamien@gmail.com>
2021-07-10 17:12:16 +0800

    feat: kitten

---
+++ java/Kitten.java
@@ -0,0 +1,43 @@
+
+// https://open.kattis.com/problems/kitten
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.util.HashMap;
+
+public class Kitten {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out);
+        int k = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        while (true) {
+            String[] input = br.readLine().split(" ");
+            int parent = Integer.parseInt(input[0]);
+            if (parent != -1) {
+                for (int i = 1; i < input.length; i++) {
+                    map.put(Integer.parseInt(input[i]), parent);
+                }
+            } else {
+                break;
+            }
+        }
+        int s = k;
+        while (true) {
+            pw.printf("%d ", s);
+            s = map.get(s);
+            if (!map.containsKey(s)) {
+                break;
+            }
+        }
+        pw.println(s);
+        pw.close();
+        br.close();
+    }
+}
