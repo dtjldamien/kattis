3710bda9366333ce36b23fc054887b47342ca9a7
Damien <dtjldamien@gmail.com>
2021-07-22 22:52:25 +0800

    feat: false security

---
+++ java/FalseSecurity.java
@@ -0,0 +1,230 @@
+
+// https://open.kattis.com/problems/falsesecurity
+import java.util.StringTokenizer;
+import java.io.BufferedReader;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.OutputStream;
+import java.lang.StringBuilder;
+
+public class FalseSecurity {
+
+    public static void main(String[] args) {
+        Kattio io = new Kattio(System.in, System.out);
+        while (io.hasMoreTokens()) {
+            String inputMessage = io.getWord();
+            StringBuilder sbMorseCode = new StringBuilder();
+            StringBuilder sbLength = new StringBuilder();
+            for (int i = 0; i < inputMessage.length(); i++) {
+                String morseCode = getMorseCode(inputMessage.charAt(i));
+                sbMorseCode.append(morseCode);
+                sbLength.append(morseCode.length());
+            }
+            String lengths = sbLength.toString();
+            StringBuilder sbOutputMessage = new StringBuilder();
+            int idx = sbMorseCode.length();
+            for (int i =  0; i < lengths.length(); i++) {
+                int currLength = Character.getNumericValue(lengths.charAt(i));
+                sbOutputMessage.append(getText(sbMorseCode.substring(idx - currLength, idx)));
+                idx = idx - currLength;
+            }
+            io.println(sbOutputMessage.reverse().toString());
+        }
+        io.close();
+    }
+
+    public static String getMorseCode(char text) {
+        switch (text) {
+            case 'A':
+                return ".-";
+            case 'B':
+                return "-...";
+            case 'C':
+                return "-.-.";
+            case 'D':
+                return "-..";
+            case 'E':
+                return ".";
+            case 'F':
+                return "..-.";
+            case 'G':
+                return "--.";
+            case 'H':
+                return "....";
+            case 'I':
+                return "..";
+            case 'J':
+                return ".---";
+            case 'K':
+                return "-.-";
+            case 'L':
+                return ".-..";
+            case 'M':
+                return "--";
+            case 'N':
+                return "-.";
+            case 'O':
+                return "---";
+            case 'P':
+                return ".--.";
+            case 'Q':
+                return "--.-";
+            case 'R':
+                return ".-.";
+            case 'S':
+                return "...";
+            case 'T':
+                return "-";
+            case 'U':
+                return "..-";
+            case 'V':
+                return "...-";
+            case 'W':
+                return ".--";
+            case 'X':
+                return "-..-";
+            case 'Y':
+                return "-.--";
+            case 'Z':
+                return "--..";
+            case '_':
+                return "..--";
+            case ',':
+                return ".-.-";
+            case '.':
+                return "---.";
+            case '?':
+                return "----";
+            default:
+                return "";
+        }
+    }
+
+    public static char getText(String morseCode) {
+        switch (morseCode) {
+            case ".-":
+                return 'A';
+            case "-...":
+                return 'B';
+            case "-.-.":
+                return 'C';
+            case "-..":
+                return 'D';
+            case ".":
+                return 'E';
+            case "..-.":
+                return 'F';
+            case "--.":
+                return 'G';
+            case "....":
+                return 'H';
+            case "..":
+                return 'I';
+            case ".---":
+                return 'J';
+            case "-.-":
+                return 'K';
+            case ".-..":
+                return 'L';
+            case "--":
+                return 'M';
+            case "-.":
+                return 'N';
+            case "---":
+                return 'O';
+            case ".--.":
+                return 'P';
+            case "--.-":
+                return 'Q';
+            case ".-.":
+                return 'R';
+            case "...":
+                return 'S';
+            case "-":
+                return 'T';
+            case "..-":
+                return 'U';
+            case "...-":
+                return 'V';
+            case ".--":
+                return 'W';
+            case "-..-":
+                return 'X';
+            case "-.--":
+                return 'Y';
+            case "--..":
+                return 'Z';
+            case "..--":
+                return '_';
+            case ".-.-":
+                return ',';
+            case "---.":
+                return '.';
+            case "----":
+                return '?';
+            default:
+                return ' ';
+        }
+    }
+}
+
+class Kattio extends PrintWriter {
+    public Kattio(InputStream i) {
+        super(new BufferedOutputStream(System.out));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public Kattio(InputStream i, OutputStream o) {
+        super(new BufferedOutputStream(o));
+        r = new BufferedReader(new InputStreamReader(i));
+    }
+
+    public boolean hasMoreTokens() {
+        return peekToken() != null;
+    }
+
+    public int getInt() {
+        return Integer.parseInt(nextToken());
+    }
+
+    public double getDouble() {
+        return Double.parseDouble(nextToken());
+    }
+
+    public long getLong() {
+        return Long.parseLong(nextToken());
+    }
+
+    public String getWord() {
+        return nextToken();
+    }
+
+    private BufferedReader r;
+    private String line;
+    private StringTokenizer st;
+    private String token;
+
+    private String peekToken() {
+        if (token == null)
+            try {
+                while (st == null || !st.hasMoreTokens()) {
+                    line = r.readLine();
+                    if (line == null)
+                        return null;
+                    st = new StringTokenizer(line);
+                }
+                token = st.nextToken();
+            } catch (IOException e) {
+            }
+        return token;
+    }
+
+    private String nextToken() {
+        String ans = peekToken();
+        token = null;
+        return ans;
+    }
+}
