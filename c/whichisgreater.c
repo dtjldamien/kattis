83c419e81fffd06dd1f7f8254ef7160be870c180
Damien Tan <damien.tan@workato.com>
2022-07-12 21:33:11 +0800

    feat: whichisgreater

---
+++ c/whichisgreater.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+  
+int main()
+{
+  int a;
+  scanf("%d", &a);
+  int b;
+  scanf("%d", &b);
+
+  if (a > b)
+  {
+    printf("%d", 1);
+  }
+  else
+  {
+    printf("%d", 0);
+  }
+}
